74337703540027c876555a05a2b7b59ea7070a07
gspetillo <gspetillo@gmail.com>
2022-08-01 20:59:45 -0300

    feat: add first seriesquotes version

---
+++ src/pages/home/script.js
@@ -0,0 +1,40 @@
+const verse = document.querySelector('#verse h2');
+const author = document.querySelector('#verse h3');
+let books = null;
+const randomBook = null;
+const randomChapter = null;
+
+const fetchQuote = async () => {
+    const url = `https://web-series-quotes-api.deta.dev/quote/`;
+    const response = await fetch(url, headers = {
+        'Accept': `application/json`
+    });
+    let data = await response.json();
+    quote = data[0]
+    console.log(quote)
+    verse.innerHTML = '"'+quote.quote+'"'
+    if(quote.author){
+
+        author.innerHTML = "- "+quote.author+", "+formatSeries(quote.series)
+    }else{
+        author.innerHTML = "- "+formatSeries(quote.series)
+    }
+}
+
+const formatSeries = (series) => {
+    newSeriesName = series[0].toUpperCase();
+    console.log(series)
+    for(let i=1; i<series.length; i++){
+        if(series[i] === '_'){
+            newSeriesName += ' '
+        }else if(series[i-1] && (series[i-1]== ' ' || series[i-1] == '_')){
+            newSeriesName += series[i].toUpperCase()
+        }else{
+            newSeriesName += series[i]
+        }
+        console.log(newSeriesName[i], series[i])
+    }
+    return newSeriesName;
+}
+
+fetchQuote()
